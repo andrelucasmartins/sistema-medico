1280f39c030472cc7fa1e19fcb86e1dc7e14f27a
andrelucasmartins <andrelucasmartins@hotmail.com>
2020-03-11 07:17:24 -0300

    Sistema Médico - first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,106 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center mt-5">Sistema Médico</h1>
+          <h2>Sintomas</h2>
+          <div class="form-check form-check-inline" v-for="(name, index) in all_sintomas" :key="index">
+            <input class="form-check-input" type="checkbox" :id="index" :value="index">
+            <label class="form-check-label" :for="index">{{ name }}</label>
+          </div>
+          <hr>
+          <h2>Resultado</h2>
+          <div>{{  }}</div>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'app',
+  result: '',
+  count: 0,
+  methods: {
+    countSintomas (count) {
+      switch (count) {
+        case count === 100:
+          this.result = 'Gripe'
+          break
+        case count === 110:
+          this.result = 'Gripe'
+          break
+        case count === 111:
+          this.result = 'Gripe'
+          break
+        default:
+          this.result = 'Insira mais dados'
+          break
+      }
+    }
+  },
+  data () {
+    return {
+      all_sintomas: {
+        1: 'Febre alta',
+        2: 'Dor de cabeça',
+        3: 'Dor_nos musculos',
+        4: 'Dor_de_articulacoes',
+        5: 'calafrios',
+        6: 'tosse',
+        7: 'Secreção nasal',
+        8: 'Falta ar',
+        9: 'Coceira pele',
+        10: 'Conjuntivite',
+        11: 'Automento gânglios'
+      },
+      doencas: [
+        {
+          name: 'gripe',
+          sintomas: {
+            1: 'Febre alta',
+            2: 'Dor de cabeça',
+            3: 'Dor_nos musculos',
+            5: 'calafrios',
+            6: 'tosse',
+            7: 'Secreção nasal'
+          }
+        },
+        {
+          name: 'gripe Suína',
+          sintomas: {
+            1: 'Febre alta',
+            2: 'Dor de cabeça',
+            3: 'Dor_nos musculos',
+            4: 'Dor_de_articulacoes',
+            5: 'calafrios',
+            6: 'tosse',
+            8: 'Falta ar'
+          }
+        },
+        {
+          name: 'Zica',
+          sintomas: {
+            1: 'Febre alta',
+            2: 'Dor de cabeça',
+            3: 'Dor_nos musculos',
+            4: 'Dor_de_articulacoes',
+            9: 'Coceira pele',
+            10: 'Conjuntivite',
+            11: 'Automento gânglios'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'popper.js'
 
 Vue.config.productionTip = false
 
